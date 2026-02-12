0541d3082278f021955e845b30b03c3d9b2e04ec
JOTHIKA <jothikakanthuluvaseenivasan@gmail.com>
2026-02-12 16:23:03 +0530

    Update HeroSection.jsx

--- client/src/components/user/blog/HeroSection.jsx
+++ client/src/components/user/blog/HeroSection.jsx
@@ -5,7 +5,7 @@ import Filters from './Filters'
 const HeroSection = () => {
 
   return (
-    <section className=" bg-[url('./blogPageThumbnail.png')] bg-center h-70 md:h-100 ">
+    <section className=" bg-[url('/blogPageThumbnail.png')] bg-center h-70 md:h-100 ">
         <HeroContent/>
         <Filters/>
     </section>
